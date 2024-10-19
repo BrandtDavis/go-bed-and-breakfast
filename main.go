dac76715f95e9559a04e3eb4615596643fbe04cd
Brandt Davis <brandtd77@gmail.com>
2024-10-19 17:42:05 -0600

    Add new route, addValues function

--- main.go
+++ main.go
@@ -5,16 +5,30 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	x, y := 2, 2
+	mySum := addValues(x, y)
+	fmt.Fprintf(w, "This is the about page, and %d + %d is %d", x, y, mySum)
+}
+
+// addValues adds 2 integers, and returns the sum
+func addValues(x, y int) int {
+	return x + y
+}
+
+// main is the main application function
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello!")
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-		if err != nil {
-			fmt.Println("[Error]:", err)
-		}
-
-		fmt.Printf("Number of bytes written: %d\n", n)
-	})
-
-	http.ListenAndServe(":8080", nil)
+	fmt.Printf("Starting application on port %s", portNumber)
+	http.ListenAndServe(portNumber, nil)
 }
