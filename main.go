4bae7a3ec2f3b3fdcc8a99a915026f85c35f1a59
Brandt Davis <brandtd77@gmail.com>
2024-10-19 17:20:48 -0600

    Add basic listener and handler

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprintf(w, "Hello!")
+
+		if err != nil {
+			fmt.Println("[Error]:", err)
+		}
+
+		fmt.Printf("Number of bytes written: %d\n", n)
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
